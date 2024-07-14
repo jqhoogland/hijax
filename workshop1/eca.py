929dd93f9a0584dab9c58deb672c6e4326b714d3
Jesse Hoogland <jessequinten@gmail.com>
2024-07-13 21:00:05 -0700

    ADD stack solution

--- workshop1/eca.py
+++ workshop1/eca.py
@@ -89,31 +89,23 @@ def simulate(
     rule_table = rule_bits.reshape(2, 2, 2)
 
     # parse initial state
-    init_state = jnp.asarray(init_state, dtype=np.uint8)
-    (width,) = init_state.shape
 
-    # accumulate output into this array
-    # extra width is to implement wraparound with slicing
-    history = jnp.zeros((height, width+2), dtype=jnp.uint8)
-
-    # first row
-    history = history.at[0, 1:-1].set(init_state)
-    history = history.at[0, 0].set(init_state[-1])
-    history = history.at[0, -1].set(init_state[0])
+    init_state = jnp.pad(init_state, 1, mode='wrap')
+    history = [init_state]
     
     # remaining rows
     for step in tqdm.trange(1, height):
-        # apply rules
-        history = history.at[step, 1:-1].set(rule_table[
-            history[step-1, 0:-2],
-            history[step-1, 1:-1],
-            history[step-1, 2:],
-        ])
-        # sync edges
-        history = history.at[step, 0].set(history[step, -2])
-        history = history.at[step, -1].set(history[step, 1])
+        init_state = jnp.pad(rule_table[
+            init_state[0:-2],
+            init_state[1:-1],
+            init_state[2:],
+        ], 1, mode='wrap')
+        
+        history.append(init_state)
 
     # return a view of the array without the width padding
+    history = jnp.stack(history)
+
     return history[:, 1:-1]
 
 
